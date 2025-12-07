c44708df53862a55d907d7798c2009e37463e9f3
anon <anon@example.com>
2025-12-07 18:42:27 +0100

    cleaned

--- print.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-c_arr = np.logspace(-3,1,6)
-
-print(c_arr)
--- utils/save_json.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def save_json(fname,obj):
-         with open(fname, "w") as f:
-                json.dump(obj,f)
